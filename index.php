e283341e08acd77a6ed2696fb014eb171ec28e68
erisilva <erivelton.contagem@gmail.com>
2024-05-22 13:58:12 -0300

    fix

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+<title>Redirecionando</title>
+    <meta http-equiv="refresh" content="0; url=http://castracao.contagem.mg.gov.br/castracao/public/">
+    <meta charset="utf-8">
+    <meta name="robots" content="noindex, nofollow">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+    <link rel="manifest" href="site.webmanifest">
+
+    <title>Secretaria Municipal de Saúde - Contagem/MG</title>
+
+    <style>
+        html,
+        body {
+            height: 100%;
+        }
+
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            color: #B0BEC5;
+            display: table;
+            font-weight: 100;
+            font-family: 'Arial';
+        }
+
+        .container {
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+        }
+
+        .content {
+            text-align: center;
+            display: inline-block;
+        }
+
+        .title {
+            font-size: 72px;
+            margin-bottom: 40px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="content">
+            <div class="title">Secretaria Municipal de Saúde - Contagem/MG</div>
+            <p>Redirecionamento para <a href="http://castracao.contagem.mg.gov.br/castracao/public/"></a>link</p>
+        </div>
+    </div>
+</body>
+
+</html>
